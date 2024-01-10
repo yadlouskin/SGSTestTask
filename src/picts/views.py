b5bb9b9c263e7c5d19ba753f85f2306a08bab68c
Nick Yadlouski <yadlouskin@gmail.com>
2024-01-10 16:45:11 +0300

    Add ListView to show list of elements of Picts model

--- src/picts/views.py
+++ src/picts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+
+from .models import Picts
 
 # Create your views here.
+
+class PictsList(ListView):
+    template_name = 'picts/picts_list.html'
+    model = Picts
