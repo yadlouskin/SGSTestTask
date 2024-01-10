4584feaf8672bbc95199917bf2718a10ffbe6e5c
Nick Yadlouski <yadlouskin@gmail.com>
2024-01-11 00:34:48 +0300

    Use created DataMixin in views and templates

--- src/picts/views.py
+++ src/picts/views.py
@@ -3,18 +3,22 @@ from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 
 from .models import Picts
+from .utils import DataMixin
 
 # Create your views here.
 
-class PictsList(ListView):
+class PictsList(DataMixin, ListView):
     template_name = 'picts/picts_list.html'
+    page_header = 'List of all pictures'
     model = Picts
 
-class PictDetails(DetailView):
+class PictDetails(DataMixin, DetailView):
     template_name = 'picts/pict_details.html'
+    page_header = 'Details of picture'
     model = Picts
 
-class PictAdd(CreateView):
+class PictAdd(DataMixin, CreateView):
     template_name = 'picts/pict_add.html'
+    page_header = 'Add a new picture'
     model = Picts
     fields = ["title", "pict", "description", "slug"]
