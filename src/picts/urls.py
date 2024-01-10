1edd0c3331784f72e038c1077271bb15f3ddc23a
Nick Yadlouski <yadlouskin@gmail.com>
2024-01-10 23:09:50 +0300

    Add CreateView functionality

--- src/picts/urls.py
+++ src/picts/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.PictsList.as_view()),
+    path('add/', views.PictAdd.as_view(), name='pict_add'),
     path('details/<slug:slug>/', views.PictDetails.as_view(), name='pict'),
 ]
--- src/picts/views.py
+++ src/picts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
 
 from .models import Picts
 
@@ -12,3 +13,8 @@ class PictsList(ListView):
 class PictDetails(DetailView):
     template_name = 'picts/pict_details.html'
     model = Picts
+
+class PictAdd(CreateView):
+    template_name = 'picts/pict_add.html'
+    model = Picts
+    fields = ["title", "pict", "description", "slug"]
