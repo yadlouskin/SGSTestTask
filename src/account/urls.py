cc3dd35f285536957995d3924e610c44b62418ff
Nick Yadlouski <yadlouskin@gmail.com>
2024-01-12 16:56:12 +0300

    Add user main page to work with user functionality

--- src/account/urls.py
+++ src/account/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.forms import AuthenticationForm
+from django.views.generic.base import TemplateView
 
 
 urlpatterns = [
@@ -10,4 +11,8 @@ urlpatterns = [
         extra_context={"page_header": "Login"}
     ), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('user/', TemplateView.as_view(
+        template_name = 'account/main_user.html',
+        extra_context={"page_header": "User main page"}
+    ), name='main_user')
 ]
