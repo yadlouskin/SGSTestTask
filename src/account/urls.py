333fc7a248cef87e329d53ef671766f5e3413b3f
Nick Yadlouski <yadlouskin@gmail.com>
2024-01-12 17:25:51 +0300

    Add password change functionality

--- src/account/urls.py
+++ src/account/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView
 from django.contrib.auth.forms import AuthenticationForm
 from django.views.generic.base import TemplateView
 
@@ -12,7 +12,16 @@ urlpatterns = [
     ), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('user/', TemplateView.as_view(
-        template_name = 'account/main_user.html',
+        template_name='account/main_user.html',
         extra_context={"page_header": "User main page"}
-    ), name='main_user')
+    ), name='main_user'),
+
+    path('password-change/', PasswordChangeView.as_view(
+        template_name='account/password_change_form.html',
+        extra_context={"page_header": "Change your password"}
+    ), name='password_change'),
+    path('password-change/done/', PasswordChangeDoneView.as_view(
+        template_name='account/password_change_done.html',
+        extra_context={"page_header": "Password changed"}
+    ), name='password_change_done'),
 ]
