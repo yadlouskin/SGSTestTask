f33f5373f7379d483460d3e445604a8e2c908760
Nick Yadlouski <yadlouskin@gmail.com>
2024-01-13 12:46:17 +0300

    Add redirection from empty url
    Remove unused logic (added in 961c16c23c0c88a10657bbcd56041a7f3eca4118),
    Because approach to handle users was changed and now application uses roles

--- src/sgsproj/urls.py
+++ src/sgsproj/urls.py
@@ -16,16 +16,16 @@ Including another URLconf
 """
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.contrib.auth.decorators import permission_required
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 from sgsproj import settings
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('admin/', permission_required(admin.site.urls), name='admin'),
     path('picts/', include('picts.urls')),
     path('account/', include('account.urls')),
+    path('', RedirectView.as_view(url='/picts/', permanent=True))
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
